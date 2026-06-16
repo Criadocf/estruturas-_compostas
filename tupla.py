9e0d4166abc43b42c55e1a63b2a5e459d98b5c77
kaleb <kleberjrmmxvi@gmail.com>
2026-06-16 08:53:24 -0300

    tupla

---
+++ tupla.py
@@ -0,0 +1,9 @@
+#a tupla é imutável, ou seja nao posso deletar, adicionar, alterar...
+
+tuplita = (True, 1546, 1.4, ['kajota, 1.83'])
+
+print(tuplita)
+
+print(type(tuplita))
+
+print(tuplita[-1])
