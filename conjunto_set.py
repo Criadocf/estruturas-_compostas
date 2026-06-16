4fd3222fcf425aa2bada3e5a54ed9a00d874429f
kajota <kleberjrmmxvi@gmail.com>
2026-06-16 17:27:00 -0300

    conjunto/set

---
+++ conjunto_set.py
@@ -0,0 +1,9 @@
+#conjunto nao repete os elementos e tambem a ordem dos elementos sempre
+# e aleatoria.
+#set nao aceita lista elemento.
+
+conjunto = {1993,"kleber", True, 1.85, 1.85,  ('kaleb', 1.10)}
+
+print(conjunto)
+
+print(len(conjunto))
